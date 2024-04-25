9924332708ecf44bda783e363f55f5308689728b
renzhamin <renzhamin@gmail.com>
2024-04-25 23:42:51 +0600

    added ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import NavBar from "@/components/navbar"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
+import { cn } from "@/lib/utils"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-
-import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
-import NavBar from "@/components/navbar"
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 export const metadata: Metadata = {
@@ -18,22 +20,32 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body
-                className={cn(
-                    "min-h-screen bg-background font-sans antialiased",
-                    inter.variable,
-                )}
-            >
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+                variables: {
+                    colorBackground: "hsl(222.2 84% 9%)",
+                },
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={cn(
+                        "min-h-screen bg-background font-sans antialiased",
+                        inter.variable,
+                    )}
                 >
-                    <NavBar />
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                    >
+                        <NavBar />
+                        {children}
+                        <Toaster />
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     )
 }
